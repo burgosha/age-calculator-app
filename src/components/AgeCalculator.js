de3896ac14dd03fd97b558f3800f48077722d4a9
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-05-16 19:38:22 -0300

    label styles when error

--- src/components/AgeCalculator.js
+++ src/components/AgeCalculator.js
@@ -53,96 +53,7 @@ function AgeCalculator() {
         setAge(calculateAge(day, month, year));
       }
     };
-    // //START
-    // //CALCULATE CURRENT DATE
-    // const currDate = new Date();
-    // const currYYYY = currDate.getUTCFullYear();
-    // // const currMM = currDate.getUTCMonth() + 1;
-    // const currDD = currDate.getUTCDate();
-    // //HOOKS
-    // const [userBirth, setUserBirth] = useState({
-    //     year: "--",
-    //     month: "--",
-    //     day: "--"
-    // });
-    // const [userAge, setUserAge] = useState({
-    //     years: "--",
-    //     months: "--",
-    //     days: "--"
-    // });
-    // const [errors, setErrors] = useState({invalidDay: false, invalidMonth: false, invalidYear: false, invalideDate: false})
-    // //HANDLE USER'S BIRTH
-    // const handleChange = (event) => {
-    //     const y = Number(document.getElementById("year").value);
-    //     const m = Number(document.getElementById("month").value);
-    //     const d = Number(document.getElementById("day").value);
-    //     const monthDays = new Date(y, m, 0).getDate();
-    //     if (d <= monthDays && y <= currYYYY && m < 12) {
-    //         setUserBirth({
-    //             ...userBirth,
-    //             [event.target.name] : event.target.value
-    //         });
-    //     } else {
-    //         setUserBirth({
-    //             ...userBirth,
-    //             [event.target.name] : "--"
-    //         });
-    //         setUserAge({
-    //             ...userAge,
-    //             years: "--",
-    //             months: "--",
-    //             days: "--"
-    //         })
-    //         // Add toggle visibility
-    //         if(monthDays < d) {
-    //             setErrors({invalidDay: true})
-    //         } else if (12 < m) {
-    //             setErrors({invalidMonth: true})
-    //         } else if (currYYYY < y) {
-    //             console.log("Must be in the past")
-    //         }
-    //     }
-    // };
-    // //HANDLE SUBMIT - CALCULATE AGE
-    // const handleSubmit = () => {
-    //     const dob = new Date(userBirth.year, userBirth.month, userBirth.day);
-    //     const diff = new Date(Date.now() - dob.getTime());
-    //     if(Math.abs(diff.getUTCMonth() + 1) < 12) {
-    //         setUserAge({
-    //             ...userAge,
-    //             years : Math.abs(diff.getUTCFullYear() - 1970),
-    //             months : Math.abs(diff.getUTCMonth() + 1),
-    //             days: Math.abs(diff.getUTCDate() - currDD)
-    //         });
-    //     } else if (Math.abs(diff.getUTCMonth() + 1) === 12 && Number(userBirth.year) !== Number(currYYYY)) {
-    //         setUserAge({
-    //             ...userAge,
-    //             years : Math.abs(diff.getUTCFullYear() - 1970) + 1,
-    //             months : 0,
-    //             days: Math.abs(diff.getUTCDate() - currDD)
-    //         });
-    //     } else if (Math.abs(diff.getUTCMonth() + 1) === 12 && Number(userBirth.year) === Number(currYYYY)) {
-    //         setUserAge({
-    //             ...userAge,
-    //             years: 0,
-    //             months: 0,
-    //             days: Math.abs(diff.getUTCDate() - currDD)
-    //         })
-    //     }
-    // };
-    // //OTHER CONTROLS
-    // useEffect(() => {
-    //     const keyDownHandler = e => {
-    //         if(e.key === 'Enter') {
-    //             handleSubmit();
-    //         }
-    //     };
-    //     document.addEventListener('keydown', keyDownHandler);
-    //     return () => {
-    //         document.removeEventListener('keydown', keyDownHandler);
-    //     }
-    // });
-
+    
     return(
         <div className="container">
             <form onSubmit={handleSubmit}>
@@ -187,9 +98,27 @@ function AgeCalculator() {
                   ? <span className="error-type">Must be a valid date</span>
                   : ""}
                 </label>
-                <label>Month
+                <label
+                  style={{color:
+                    (((!(day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && !(year && year > new Date().getFullYear()))) ||
+                    ((day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && !(year && year > new Date().getFullYear())) ||
+                    (!(day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())))
+                    ? "#FF5959" : 
+                    ((day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())) 
+                    ? "#FF5959"
+                    : ""
+                  }}
+                >Month
                   <input 
-                  className="date-number" 
+                  className={
+                    (((!(day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && !(year && year > new Date().getFullYear()))) ||
+                    ((day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && !(year && year > new Date().getFullYear())) ||
+                    (!(day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())))
+                    ? "date-number error" : 
+                    ((day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())) 
+                    ? "date-number error"
+                    : "date-number"
+                  } 
                   type="number" 
                   placeholder="MM" 
                   id="month" 
@@ -206,8 +135,26 @@ function AgeCalculator() {
                   ? <span className="error-type">Must be a valid month</span> 
                   : ""}                  
                 </label>
-                <label>Year
-                  <input className="date-number"
+                <label
+                  style={{ color:
+                    (((!(day && (day < 1 || day > 31)) && !(month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear()))) ||
+                    (!(day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())) ||
+                    ((day && (day < 1 || day > 31)) && !(month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())))
+                    ? "#FF5959" :
+                    ((day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())) 
+                    ? "#FF5959"
+                    : ""
+                  }}
+                >Year
+                  <input className={
+                    (((!(day && (day < 1 || day > 31)) && !(month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear()))) ||
+                    (!(day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())) ||
+                    ((day && (day < 1 || day > 31)) && !(month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())))
+                    ? "date-number error" :
+                    ((day && (day < 1 || day > 31)) && (month && (month < 1 || month > 12)) && (year && year > new Date().getFullYear())) 
+                    ? "date-number error"
+                    : "date-number"
+                  }
                   type="number" 
                   placeholder="YYYY" 
                   id="year" 
