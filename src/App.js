22371a6350e709c5ecfdd59213cf88f8b999c129
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-05-23 21:22:55 -0300

    Attribution

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <AgeCalculator />
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/burgosha" target='_blank' rel='noreferrer'>Hernán Burgos</a>.
+      </div>
     </div>
   );
 }
